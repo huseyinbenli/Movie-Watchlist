4c054554a58ab91bf3966c36244945abfc0d71a1
huseyinbenli <huseyinbenli@outlook.fr>
2026-02-09 11:24:28 +0100

    save movies function imported and added an event listener

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 import { fetchMovies } from "./functions/fetchMovies.js";
+import { saveMovies } from "./functions/saveMovies.js";
+
 const formEl = document.getElementById("form");
+const mainEl = document.getElementById("search-results");
 
 formEl.addEventListener("submit", fetchMovies);
+
+mainEl.addEventListener("click", saveMovies);
