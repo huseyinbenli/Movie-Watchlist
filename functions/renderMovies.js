c638bb3272df1bb09027477df86531e0bd463dd5
huseyinbenli <huseyinbenli@outlook.fr>
2026-02-09 11:22:40 +0100

    classes added to some elements

--- functions/renderMovies.js
+++ functions/renderMovies.js
@@ -1,24 +1,22 @@
-const searchResultsEl = document.getElementById("search-results");
-
 export function renderMovies(movie) {
-  searchResultsEl.innerHTML += `
-            <div class="movie" id="${movie.imdbID}">
+  return `
+            <div class="movie" data-id="${movie.imdbID}">
               <img class="movie-img" src="${movie.Poster}" alt="${movie.Title}" />
                 <div class="movie-info-wrapper">
                     <div class="movie-title-wrapper">
                         <h2 class="movie-title">${movie.Title}</h2>
                         <i class="fa-solid fa-star"></i>
-                        <p>${movie.imdbRating}</p>
+                        <p class="movie-rating">${movie.imdbRating}</p>
                     </div>
                     <div class="addlist-wrapper">
-                        <p>${movie.Runtime}</p>
-                        <p>${movie.Genre}</p>
+                        <p class="movie-runtime">${movie.Runtime}</p>
+                        <p class="movie-genre">${movie.Genre}</p>
                         <div class="watchlist">
                         <i class="fa-solid fa-circle-plus watchlist"></i>
                         Watchlist
                         </div>
                     </div>
-                    <p>${movie.Plot}</p>
+                    <p class="movie-plot">${movie.Plot}</p>
                  </div>
             </div>
           `;
